5e7bd4617bf798f6b3e3c8bf13f442751b25898f
Mark Piper <mark.piper@colorado.edu>
2015-09-08 17:16:50 -0600

    Set cfg_directory based on os.path.realpath
    This lets the .cfg file be passed to initialize() with an absolute or
    a relative path.
    
    This commit reverts the change in 90a2ee77.

--- topoflow/utils/BMI_base.py
+++ topoflow/utils/BMI_base.py
@@ -1978,11 +1978,11 @@ class BMI_component:
         # the "." to the full CFG_file directory. (9/21/14)
         #------------------------------------------------------------
         if (self.cfg_file != None):
-            cfg_directory = os.path.dirname(self.cfg_file) + os.sep
+            cfg_directory = os.path.dirname(os.path.realpath(self.cfg_file))
             ## print 'cfg_directory =', cfg_directory
             self.cfg_directory = cfg_directory
             if (self.in_directory[0] == '.'):
-                self.in_directory = os.path.abspath('.')
+                self.in_directory = self.cfg_directory
                 
         #------------------------------------------------------
         # Expand path abbreviations: "." and "..", but NOT
